43e5ccc0083025a22590dc8011577878ac9d95c1
Yousf316 <youseframzii@outlook.com>
2024-07-14 21:32:22 +0300

    Change profile

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -22,13 +22,14 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
 
 const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <CAvatar size="md">
+          <CIcon icon={cilUser} className="me-2" />
+        </CAvatar>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Settings</CDropdownHeader>
